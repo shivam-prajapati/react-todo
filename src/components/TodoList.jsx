1fa8a900de5d54857eae941c726fdaedb40f80a9
shivamprajapatigola@gmail.com <shivamprajapatigola@gmail.com>
2024-06-19 14:03:52 +0530

    Added Case when no TODO

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,6 +2,7 @@ import TodoItem from "./TodoItem";
 import styles from "./TodoList.module.css";
 const TodoList = ({ list, delTodo, edTodo }) => {
   // console.log(list);
+  if (list.length === 0) return <div>No Tasks added currently</div>;
   return (
     <div className={styles.TodoList}>
       {list.map(({ tsk, id }) => (
